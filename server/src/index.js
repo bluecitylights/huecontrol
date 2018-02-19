6bdeee2ac8e67de19d36a082c5853fe28ca44cbd
bluecitylights <30653230+veldmanr@users.noreply.github.com>
2018-02-19 22:59:01 +0100

    Added static pages

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,9 @@ const hueControl = require('./hue.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+app.use('/static', express.static('public/static'));
+
 app.get('/api/hello', (req, res) => {
   console.log('hello called')
   res.send({ express: 'Welcome to HUE Control' });
