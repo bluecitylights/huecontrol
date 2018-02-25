248dd13c4aed7477d9f0ebee51c492ec441376f8
bluecitylights <30653230+veldmanr@users.noreply.github.com>
2018-02-25 22:56:30 +0100

    Added react-promise to support rendering functional components with promises

--- client/src/Users.js
+++ client/src/Users.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom';
 //import Lights from './Lights';
 import HueControlApi from './HueControlApi';
+import Async from 'react-promise'
 
 // const Topics = ({ match }) => (
 //   <div>
@@ -87,12 +88,15 @@ const AllUsers = (props) => (
 // )
 
 const Users = (props) => (
-  <div>
-    <h2>Users</h2>
-    <ul>{HueControlApi.allUsers().map((user, index) => (<UserLink user={user} {...props}/>))}</ul>
-    <Route path={`${props.match.url}/:id`} component={User} />
-    <Route exact path={props.match.url} render={() => <h3>Please select a user.</h3>}/>
-  </div>
+  <Async promise={HueControlApi.allUsers()} then={(users) => (
+    <div>
+      <h2>Users</h2>
+      <ul>{users.map((user, index) => (<UserLink user={user} {...props}/>))}</ul>
+      <Route path={`${props.match.url}/:id`} component={User} />
+      <Route exact path={props.match.url} render={() => <h3>Please select a user.</h3>}/>
+    </div>
+  )}
+  />
 );
 
 const User = (props) => (
